dd3a667cab74dfdd36062435314c16f7e626f9b0
marcinsukacz <marcin.sukacz@gmail.com>
2019-06-14 15:42:01 +0100

    adding and removing from the list is working

---
+++ to_do_list/js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newTaskForm = document.querySelector('#new-task-form');
+  newTaskForm.addEventListener('submit', handleTaskSubmit);
+
+  const deleteAll = document.querySelector('#delete-all');
+  deleteAll.addEventListener('click', handleDeleteAll);
+})
+
+const handleTaskSubmit = function (event) {
+  event.preventDefault();
+
+  const toDoTask = createToDoTask(event.target);
+  const toDoList = document.querySelector('#to-do-list');
+  toDoList.appendChild(toDoTask);
+
+  event.target.reset();
+}
+
+const createToDoTask = function (form) {
+  const toDoTask = document.createElement('li');
+  toDoTask.classList.add('to-do-task');
+
+  const taskName = document.createElement('h2');
+  taskName.textContent = form.taskName.value;
+  toDoTask.appendChild(taskName);
+
+  const taskImportance = document.createElement('h3');
+  taskImportance.textContent = form.taskImportance.value;
+  toDoTask.appendChild(taskImportance);
+
+  const dueDate = document.createElement('h3');
+  dueDate.textContent = form.dueDate.value;
+  toDoTask.appendChild(dueDate);
+
+  return toDoTask;
+}
+
+const handleDeleteAll = function (event) {
+  const toDoList = document.querySelector('#to-do-list');
+  toDoList.innerHTML = '';
+}
